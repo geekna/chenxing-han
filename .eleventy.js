10d7fe12d3a7ce0f4e58af63851e76ee87b3f75a
geekna <geekna@gmail.com>
2025-07-26 16:54:32 -0400

    rewrite

--- .eleventy.js
+++ .eleventy.js
@@ -27,9 +27,8 @@ module.exports = function (eleventyConfig) {
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy({
     "./src/admin/config.yml": "./admin/config.yml",
-    "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
-    "./node_modules/prismjs/themes/prism-tomorrow.css":
-      "./static/css/prism-tomorrow.css",
+    "./src/static/css/styles.css": "./static/css/styles.css",
+    "./src/static/fonts": "fonts",
   });
 
   // Copy Image Folder to /_site
